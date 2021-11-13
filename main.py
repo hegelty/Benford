013ffaf497c3f48504a61f96ad6ee947995a1d41
hegelty <73592868+hegelty@users.noreply.github.com>
2021-11-13 22:23:27 +0900

    카이제곱 분석 어켛는지 모르겠다

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# json 분할
 '''
 import ijson
 
@@ -41,27 +42,32 @@ for doc in parse_namuwiki_json(10000000, debug=False):
         output.write(doc["title"]+"\n"+doc['text']+"\n")
         output.close()
 '''
-
+# 숫자 추출
+from math import log10
+'''
 import os, re
 import time
 from threading import Thread
 
 output = open("output.txt", "w", encoding="utf-8")
-file_path="C:/Programming/namuwiki"
+file_path = "C:/Programming/namuwiki"
 file_list = os.listdir(file_path)
 file_len = len(file_list)
-cnt = [0,0,0,0,0,0,0,0,0,0]
+cnt = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 
 def worker(id):
     i = id
+    w_cnt = 0
     while i < file_len:
+        w_cnt += 1
         file_name = file_list[i]
         file = open(file_path + "/" + file_name, "r", encoding="utf-8")
         filedata = file.read()
         filedata = re.sub(
-            "\.|[0-9]{2,4}년|[0-9]{1,2}월|[0-9]{1,2}[월.][^\s]{1,2}[0-9]{1,2}일|[0-9]{4}\-[0-9]{2}-[0-9]{2}|[0-9]{1,2}시|[0-9]{1,2}시 [0-9]{1,2}분|[#][a-zA-Z0-9]{6}|\[youtube\([a-zA-Z0-9]+\)|\[\[http[^]]+|\[\[파일:[^]]+|<[a-zA-Z0-9]+>|<[\-a-zA-Z0-9][a-zA-Z0-9]+>|1[sS][tT]|2[nN][dD]|3[rR][dD]|[4-9][tT][hH]|\{\{\{[+-][0-9]",
+            "\.|[0-9]{2,4}년|[0-9]{1,2}월|[0-9]{1,2}[월.][^\s]{1,2}[0-9]{1,2}일|[0-9]{4}\-[0-9]{2}-[0-9]{2}|[0-9]{1,2}시|[0-9]{1,2}시 [0-9]{1,2}분|[#][a-zA-Z0-9]{6}|\[youtube\([a-zA-Z0-9]+\)|\[\[http[^]]+|\[\[파일:[^]]+|<[a-zA-Z0-9]+>|<[\-a-zA-Z0-9][a-zA-Z0-9]+>|1[sS][tT]|2[nN][dD]|3[rR][dD]|[4-9][tT][hH]|\{\{\{[+-][0-9]|[0-9.]+%|[0-9.]+[ ~][0-9.]+%",
             "", filedata, flags=re.MULTILINE)
-        filedata = filedata.replace("0","")
+        filedata = filedata.replace("0", "")
         results = re.findall(r'\d+', filedata)
         file.close()
 
@@ -69,9 +75,11 @@ def worker(id):
         for result in results:
             file_result[int(result[0])] += 1
         for j in range(10):
-            cnt[j]+=file_result[j]
-        output.write(f"{file_name[:-4]} : {str(file_result)}\n")
+            cnt[j] += file_result[j]
+        # output.write(f"{file_name[:-4]} : {str(file_result)}\n")
         i += 20
+        if w_cnt % 100 == 0: print(f"{id}: {w_cnt}")
+
 
 start = time.time()
 threads = []
@@ -85,3 +93,18 @@ for thread in threads:
 print(f"{time.time() - start:.5f}sec")
 print(str(cnt))
 print(len(file_list))
+'''
+import pandas as pd
+from scipy import stats
+cnt = [14377845, 11271107, 6299994, 4149820, 3860590, 2433708, 2030557, 1853162, 1581374]
+actual = []
+for i in range(9):
+    actual.append(cnt[i]/sum(cnt)*100)
+print(actual)
+ex = []
+for i in range(1,10):
+      ex.append(log10(1+1/i)*100)
+print(ex)
+print(sum(actual)-sum(ex))
+chis = stats.chisquare(actual, ex)
+print(chis)
